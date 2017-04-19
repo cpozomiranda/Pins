7afe4a2a425d3e44e2060cce9239dd252735dfd9
Cesar Pozo <c.pozomiranda@gmail.com>
2017-04-19 01:21:01 -0300

    names

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,5 +2,6 @@ class Pin < ApplicationRecord
 	
 	validates :photo, presence: true
 	validates :description, presence: true
+	validates :name, presence: true
 	
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170418234445) do
+ActiveRecord::Schema.define(version: 20170419014947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170418234445) do
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "name"
   end
 
 end
